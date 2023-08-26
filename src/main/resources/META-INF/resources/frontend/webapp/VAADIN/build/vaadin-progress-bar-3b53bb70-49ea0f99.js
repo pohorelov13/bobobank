94aa96590e8836d038cae7dfa786f4708e4b7d68
Your Name <you@example.com>
2023-08-26 23:44:13 +0300

    modify web-inf

---
+++ src/main/resources/META-INF/resources/frontend/webapp/VAADIN/build/vaadin-progress-bar-3b53bb70-49ea0f99.js
@@ -0,0 +1,1 @@
+import{U as r}from"./indexhtml-f17d019e.js";const e={tagName:"vaadin-progress-bar",displayName:"ProgressBar",elements:[{selector:"vaadin-progress-bar::part(bar)",displayName:"Bar",properties:[r.backgroundColor]},{selector:"vaadin-progress-bar::part(value)",displayName:"Value",properties:[r.backgroundColor]}]};export{e as default};
