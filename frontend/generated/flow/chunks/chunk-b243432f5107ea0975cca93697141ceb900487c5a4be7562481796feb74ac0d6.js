f06463dd886844ed0d8ff3f7aa87f6c38130ff6e
Your Name <you@example.com>
2023-08-26 22:11:52 +0300

    Modify pom

---
+++ frontend/generated/flow/chunks/chunk-b243432f5107ea0975cca93697141ceb900487c5a4be7562481796feb74ac0d6.js
@@ -0,0 +1,11 @@
+import '@vaadin/polymer-legacy-adapter/style-modules.js';
+import '@vaadin/text-field/theme/lumo/vaadin-text-field.js';
+import '@vaadin/form-layout/theme/lumo/vaadin-form-layout.js';
+import '@vaadin/password-field/theme/lumo/vaadin-password-field.js';
+import '@vaadin/vertical-layout/theme/lumo/vaadin-vertical-layout.js';
+import '@vaadin/tooltip/theme/lumo/vaadin-tooltip.js';
+import '@vaadin/form-layout/theme/lumo/vaadin-form-item.js';
+import '@vaadin/button/theme/lumo/vaadin-button.js';
+import 'Frontend/generated/jar-resources/buttonFunctions.js';
+import '@vaadin/notification/theme/lumo/vaadin-notification.js';
+import 'Frontend/generated/jar-resources/flow-component-renderer.js';
